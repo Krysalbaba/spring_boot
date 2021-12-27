6ec10195f4655d89a7a430d4faeffaf8f9aeca00
nch <80524699+Krysalbaba@users.noreply.github.com>
2021-12-27 20:25:40 +0800

    docker插件集成

--- src/main/java/com/java/nie/NieApplication.java
+++ src/main/java/com/java/nie/NieApplication.java
@@ -1,6 +1,8 @@
 package com.java.nie;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,18 +11,16 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @MapperScan("com.java.nie.mapper")
 @EnableCaching
-public class NieApplication  implements CommandLineRunner {
+public class NieApplication {
+
+	private final static Logger logger = LoggerFactory.getLogger(NieApplication.class);
 
 	public static void main(String[] args) {
 		try {
 			SpringApplication.run(NieApplication.class, args);
+			logger.info("启动成功！！！！！！！！！！");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.err.println("启动成功！！！！！！！！！！");
 	}
 }
