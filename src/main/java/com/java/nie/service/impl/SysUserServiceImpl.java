fa9fe2f261521a02f735b2e6229aff726e7b60bf
nie <1924596337@qq.com>
2022-06-18 11:52:58 +0800

    git测试

---
+++ src/main/java/com/java/nie/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.java.nie.service.impl;
+
+import com.java.nie.domain.SysUser;
+import com.java.nie.mapper.SysUserMapper;
+import com.java.nie.service.ISysUserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 系统用户表 服务实现类
+ * </p>
+ *
+ * @author nie
+ * @since 2022-06-16
+ */
+@Service
+public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> implements ISysUserService {
+
+}
