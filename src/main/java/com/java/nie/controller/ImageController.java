b36eb20d72e4921a409a02f29b3be2fcf67aea24
nch <80524699+Krysalbaba@users.noreply.github.com>
2022-11-21 16:01:29 +0800

    照片地址获取

---
+++ src/main/java/com/java/nie/controller/ImageController.java
@@ -0,0 +1,61 @@
+package com.java.nie.controller;
+
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+
+@RestController
+@RequestMapping("/")
+public class ImageController {
+    public static void main(String[] args) throws Exception {
+         File file = new File("D:\\project\\images\\20221121155642.jpg");
+         printImageTags(file);
+
+    }
+
+    /**
+     * 15      * 读取照片里面的信息
+     * 16
+     */
+    private static void printImageTags(File file) throws ImageProcessingException, Exception {
+        Metadata metadata = ImageMetadataReader.readMetadata(file);
+        for (Directory directory : metadata.getDirectories()) {
+            for (Tag tag : directory.getTags()) {
+                String tagName = tag.getTagName();
+                String desc = tag.getDescription();
+                if (tagName.equals("Date/Time Original")) {
+                    System.out.println("拍摄时间: " + desc);
+                } else if (tagName.equals("GPS Latitude")) {
+                    System.err.println("纬度 : " + desc);
+                    System.err.println("纬度(度分秒格式) : "+pointToLatlong(desc));
+                } else if (tagName.equals("GPS Longitude")) {
+                    System.err.println("经度: " + desc);
+                    System.err.println("经度(度分秒格式): "+pointToLatlong(desc));
+                }
+
+                System.err.println(tagName + "================"+desc);
+            }
+        }
+    }
+
+    /**
+     * 经纬度格式  转换为  度分秒格式 ,如果需要的话可以调用该方法进行转换
+     *
+     * @param point 坐标点
+     * @return
+     */
+    public static String pointToLatlong(String point) {
+        Double du = Double.parseDouble(point.substring(0, point.indexOf("°")).trim());
+        Double fen = Double.parseDouble(point.substring(point.indexOf("°") + 1, point.indexOf("'")).trim());
+        Double miao = Double.parseDouble(point.substring(point.indexOf("'") + 1, point.indexOf("\"")).trim());
+        Double duStr = du + fen / 60 + miao / 60 / 60;
+        return duStr.toString();
+    }
+}
