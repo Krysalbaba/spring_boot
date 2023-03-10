dc72562ee707b61a9f95130de22ee385c5b27646
nie <1924596337@qq.com>
2023-03-10 16:45:18 +0800

    init

--- src/main/java/com/java/nie/controller/DemoController.java
+++ src/main/java/com/java/nie/controller/DemoController.java
@@ -1,32 +1,21 @@
 package com.java.nie.controller;
 
 
-import cn.hutool.core.date.DateField;
-import cn.hutool.core.date.DateTime;
-import cn.hutool.core.date.DateUtil;
-import com.alibaba.excel.util.DateUtils;
 import com.java.nie.config.MonthTableNameHandler;
 import com.java.nie.config.RabbitMQConfig;
 import com.java.nie.config.RedisService;
 import com.java.nie.domain.YarntaskLog;
 import com.java.nie.mapper.YarntaskLogMapper;
-import com.java.nie.utils.DateCalculationUtil;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.Resource;
-import java.io.IOException;
-import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author nie
@@ -47,7 +36,7 @@ public class DemoController {
     private RedisTemplate<String, Object> redisTemplate;
 
     @Resource
-    private YarntaskLogMapper yarntaskLogMapper ;
+    private YarntaskLogMapper yarntaskLogMapper;
 
     @GetMapping("/test1")
     public void test1() {
@@ -81,48 +70,48 @@ public class DemoController {
     @GetMapping("/test5")
     public void test5() {
         String msg = "测试fanout是否可以消费";
-        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_FANOUT_DEMO,"", msg);
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_FANOUT_DEMO, "", msg);
     }
 
 
-    private  final static String  PTC_COUNT= "ptc-count:" ;
+    private final static String PTC_COUNT = "ptc-count:";
 
     @GetMapping("/between/demo")
     public void demo1() throws ParseException {
-        List<Map<String,Object>> result = new ArrayList<>();
+        List<Map<String, Object>> result = new ArrayList<>();
         Set<String> keys = redisTemplate.keys(PTC_COUNT + "*");
-        for (String key :keys ){
-            Map<String,Object>  info=new HashMap<>();
+        for (String key : keys) {
+            Map<String, Object> info = new HashMap<>();
             Map<Object, Object> paramMap = redisService.hmget(key);
             Object startTime = paramMap.get("startTime");
             Object endTime = paramMap.get("endTime");
             Long between = between(startTime.toString(), endTime.toString());
             String[] split = key.split(":");
-            info.put("between",between);
-            info.put("ptcId",split[1]);
+            info.put("between", between);
+            info.put("ptcId", split[1]);
             result.add(info);
         }
 
-        result.sort(Comparator.comparing(o ->Long.parseLong(o.get("between").toString())));
+        result.sort(Comparator.comparing(o -> Long.parseLong(o.get("between").toString())));
         System.out.println(result);
     }
 
 
     @GetMapping("/yarnLog")
-    public void yarnLog(){
+    public void yarnLog() {
         MonthTableNameHandler.setMonth("202211");
         int insert = yarntaskLogMapper.insert(new YarntaskLog());
         System.out.println(insert);
     }
 
-    public Long between(String startTime , String endTime) throws ParseException {
+    public Long between(String startTime, String endTime) throws ParseException {
         SimpleDateFormat simpleFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date fromDate3 = simpleFormat.parse(startTime);
         Date toDate3 = simpleFormat.parse(endTime);
         long from3 = fromDate3.getTime();
         long to3 = toDate3.getTime();
-        long s = ((to3 - from3) / 1000 );
-        return s ;
+        long s = ((to3 - from3) / 1000);
+        return s;
     }
 
 }
